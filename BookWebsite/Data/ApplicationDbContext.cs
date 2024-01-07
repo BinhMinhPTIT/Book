0728e1dca030ed4dd105b89a6ee12fb8f81e3c77
BinhMinhPTIT <binhminhtt03@gmail.com>
2024-01-07 15:51:42 +0700

    Display hard data

---
+++ BookWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BookWebsite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebsite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; } // tao bang cho DB
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
+        }
+    }
+}
